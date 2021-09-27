e616b2c976d2387546e91e5d891f833211c2feb8
DaJoelATT <DaJoelATT@users.noreply.github.com>
2021-09-27 17:10:58 +0000

    Push From Script

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Making a list
+
+my_list = ["192.168.0.5",5060,"UP"]
+
+# Checking it once
+
+print("first thing on the list is a IP address",my_list[0])
+
+# checking it twice
+
+print("Second thing on my list is the port #",my_list[1])
+
+# checking it thrice
+
+print("And the last thing on the list is its state",my_list[2])
+
+# There is no 4th thing.  Seeing what happens.
+
+print("Should be an error",my_list[3])
+
+
