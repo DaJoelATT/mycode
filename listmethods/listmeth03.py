7ca137d2b329e3fcc62d1101ba75874313fb7cbb
DaJoelATT <DaJoelATT@users.noreply.github.com>
2021-09-27 18:48:31 +0000

    Push From Script

---
+++ listmethods/listmeth03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+## Lets get a list, call it fruit
+# TO get a list, it is imput.
+# and to get a lot of list, it is a longer statement
+
+fruits = []
+
+for x in range(5):
+    fruits.append(input("Name a fruit"))
+
+print(fruits)
+
